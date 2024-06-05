643680d9f31dc3e568c56a64ca932630ad812e9c
Garis Putih Teknologi <140621434+garisputihteknologi@users.noreply.github.com>
2024-06-05 16:58:46 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 html_static_path = ['_static']
+html_logo = "_static/logo_rw.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
