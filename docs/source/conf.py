7e16a8ca3add860b56892ac774dde32fcb030a0b
Garis Putih Teknologi <140621434+garisputihteknologi@users.noreply.github.com>
2024-06-05 17:13:54 +0900

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 
 html_static_path = ['_static']
-html_logo = "_static/logo.png"
+html_logo = "_static/logo-kecil.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
